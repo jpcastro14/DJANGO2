4bcc7778ae0ebab53601c9087e0893dd224f0ae4
jpcastro14 <castrovoip@gmail.com>
2024-07-31 14:58:46 -0300

    Reviewing kwargs study

--- events/serializers.py
+++ events/serializers.py
@@ -4,7 +4,7 @@ from .models import Course, Rating
 
 class RatingSerializer(serializers.ModelSerializer):
 
-    course = serializers.StringRelatedField()
+    #course = serializers.StringRelatedField()
 
     class Meta:
         extra_kwargs = { 'email':{'write_only':True}}
@@ -17,8 +17,6 @@ class RatingSerializer(serializers.ModelSerializer):
                   'email',
                   'comment',
                   'rating',
-                  'creation',
-                  'updated',
                   ]
 
 class CourseSerializer(serializers.ModelSerializer):
--- events/urls.py
+++ events/urls.py
@@ -2,9 +2,22 @@ from django.urls import path
 from .views import CoursesAPIView,CourseAPIView, RatingsAPIView, RatingAPIView
 
 urlpatterns = [
+    # Abaixo: Buscando toda a coleção de cursos
     path('courses/',CoursesAPIView.as_view(), name='courses'),
+
+    # Abaixo: Buscando um curso específico dentro de toda a coleção
     path('course/<int:pk>', CourseAPIView.as_view(), name='course'),
+
+    # Abaixo: Buscando a coleção de AVALIAÇÕES em um curso específico #
+    path('courses/<int:course_pk>/ratings', RatingsAPIView.as_view(), name='course_ratings'),
+
+    # Abaixo: Buscando uma AVALIAÇÃO específica dentro de um curso específico #
+    path('courses/<int:course_pk>/ratings/<int:rating_pk>', RatingAPIView.as_view(), name='course_rating'),
+
+    # Abaixo: Buscando toda a coleção de avaliações criadas
     path('ratings/',RatingsAPIView.as_view(),name='ratings'),
+
+    # Abaixo: Buscando uma avaliação específica de todas as avaliações criadas
     path('ratings/<int:pk>',RatingAPIView.as_view(),name='rating'),
 
 ]
